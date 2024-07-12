54fd5d69cd2338e18bdcf833c254adbdb7ea0f8e
TaylorBoyd <taylorboyd@gmail.com>
2024-07-12 17:22:42 -0400

    Finished Updates

--- helpers/GamestateHelper.py
+++ helpers/GamestateHelper.py
@@ -12,6 +12,28 @@ class GamestateHelper:
 
         return gamestate
 
+    def player_setup(self, player_id, faction):
+
+        name = self.gamestate["players"][str(player_id)]["player_name"]
+
+        if self.gamestate["setup_finished"] == "True":
+            return ("The game has already been setup!")
+
+        with open("data/factions.json", "r") as f:
+            faction_data = json.load(f)
+        self.gamestate["players"][str(player_id)].update(faction_data[faction])
+        self.update()
+        return(f"{name} is now setup!")
+
+    def setup_finished(self):
+
+        for i in self.gamestate["players"]:
+            if len(self.gamestate["players"][i]) < 3:
+                return(f"{self.gamestate["players"][i]["player_name"]} still needs to be setup!")
+
+        self.gamestate["setup_finished"] = "True"
+        return("Game setup complete")
+
     def update(self):
         with open(f"{config.gamestate_path}/{self.game_id}.json", "w") as f:
             json.dump(self.gamestate, f)
