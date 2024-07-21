a5bb2c91240df054c3ece11fa038214a7db51c8c
Fingon00 <britainfan234@gmail.com>
2024-07-20 23:55:18 -0400

    Tile setup and exploring

--- commands/setup_commands.py
+++ commands/setup_commands.py
@@ -2,6 +2,7 @@ import discord
 import config
 from discord.ext import commands
 from discord import app_commands
+from discord.ui import View, Button
 from typing import Optional, List
 from setup.GameInit import GameInit
 from helpers.GamestateHelper import GamestateHelper
@@ -13,44 +14,97 @@ import random
 class SetupCommands(commands.GroupCog, name="setup"):
     def __init__(self, bot):
         self.bot = bot
-
     
-    async def showGame(self, interaction, tileMap, game):
+
+    factionChoices = [
+        app_commands.Choice(name="Hydran Progress", value="hyd"),
+        app_commands.Choice(name="Eridian Empire", value="eri"),
+        app_commands.Choice(name="Orian Hegemony", value="ori"),
+        app_commands.Choice(name="Descendants of Draco", value="dra"),
+        app_commands.Choice(name="Mechanema", value="mec"),
+        app_commands.Choice(name="Planta", value="pla"),
+        app_commands.Choice(name="Terran Alliance", value="ter1"),
+        app_commands.Choice(name="Terran Conglomerate", value="ter2"),
+        app_commands.Choice(name="Terran Directorate", value="ter3"),
+        app_commands.Choice(name="Terran Federation", value="ter4"),
+        app_commands.Choice(name="Terran Republic", value="ter5"),
+        app_commands.Choice(name="Terran Union", value="ter6"),
+    ]
+   
+    async def on_interaction(interaction):
+        if interaction.custom_id == "showGame":
+            await interaction.response.send_message("Button clicked!")
+
+    @staticmethod 
+    async def showGame(interaction, game):
+        await interaction.response.defer(thinking=True)  
         context = Image.new("RGBA",(4160,5100),(255,255,255,0))
+        tileMap = game.get_gamestate()["board"]
         for key,value in tileMap.items():
-            context = game.drawTile(context,key, value)
+            tile_image, rotation_value = value
+            context = game.drawTile(context,key, tile_image, rotation_value)
         bytes = BytesIO()
         context.save(bytes,format="PNG")
         bytes.seek(0)
-        file = discord.File(bytes,filename="context.png")
-        await interaction.channel.send(file=file)
+        file = discord.File(bytes,filename="mapImage.png")
+        await interaction.followup.send(file=file)
+        view = View()
+        button = Button(label="Show Game",style=discord.ButtonStyle.primary, custom_id="showGame")
+        view.add_item(button)
+        await interaction.channel.send(view=view)
+    
 
 
-    @app_commands.command(name="setup_initial_tiles")
-    async def setup_initial_tiles(self, interaction: discord.Interaction, player1: discord.Member, player2: Optional[discord.Member]=None, player3: Optional[discord.Member]=None, player4: Optional[discord.Member]=None, player5: Optional[discord.Member]=None, player6: Optional[discord.Member]=None):
+
+    @app_commands.command(name="setup_game")
+    @app_commands.choices(faction1=factionChoices,faction2=factionChoices,faction3=factionChoices,faction4=factionChoices,faction5=factionChoices,faction6=factionChoices)
+    async def setup_game(self, interaction: discord.Interaction, 
+                                player1: discord.Member, faction1: app_commands.Choice[str], 
+                                player2: Optional[discord.Member]=None,faction2: Optional[app_commands.Choice[str]]=None, 
+                                player3: Optional[discord.Member]=None,faction3: Optional[app_commands.Choice[str]]=None, 
+                                player4: Optional[discord.Member]=None, faction4: Optional[app_commands.Choice[str]]=None,
+                                player5: Optional[discord.Member]=None, faction5: Optional[app_commands.Choice[str]]=None,
+                                player6: Optional[discord.Member]=None, faction6: Optional[app_commands.Choice[str]]=None):
         temp_player_list = [player1, player2, player3, player4, player5, player6]
+        temp_faction_list = [faction1, faction2, faction3, faction4, faction5, faction6]
         game = GamestateHelper(interaction.channel)
         count = 0
+        listPlayerHomes=[]
+        x = -1
         for i in temp_player_list:
-            if i != None:
+            x = x+1
+            if i != None and temp_faction_list[x] != None:
+                player = i
+                faction = temp_faction_list[x]
+                game.player_setup(player.id, faction.value)
+                home = game.get_player(player.id)["home_planet"]
+                listPlayerHomes.append(home)
                 count = count + 1
         
         listOfTilesPos = ["201", "207","205","211","203","209"]
         if count == 3:
             listOfTilesPos = [ "201","205", "209","211","203","207",]
-        listPlayerHomes = ["222","224","226","228","230","232"]
+       # listPlayerHomes = ["222","224","226","228","230","232"]
         random.shuffle(listPlayerHomes)
         listDefended = ["271","272","273","274"]
         random.shuffle(listDefended)
         mappedSectorsToPos = {}
-        mappedSectorsToPos["000"] = "sector001"
+        mappedSectorsToPos["000"] = ("001",0)
         for i in range(count):
-            mappedSectorsToPos[listOfTilesPos[i]]="sector"+listPlayerHomes[i]
-
+            rotDet = ((180 - (int(listOfTilesPos[i])-201)/2 * 60) + 360)%360
+            mappedSectorsToPos[listOfTilesPos[i]]=(listPlayerHomes[i],rotDet)
         for i in range(6-count):
-            mappedSectorsToPos[listOfTilesPos[5-i]]="sector"+listDefended[i]
-
-        await SetupCommands.showGame(self,interaction, mappedSectorsToPos, game)
+            rotDet = ((180 - (int(listOfTilesPos[5-i])-201)/2 * 60) + 360)%360
+            mappedSectorsToPos[listOfTilesPos[5-i]]=(listDefended[i],rotDet)
+        for i in range(101, 107):  
+            mappedSectorsToPos[str(i)]=("sector1back",0)
+        for i in range(201, 213):  
+             if str(i) not in listOfTilesPos: 
+                mappedSectorsToPos[str(i)]=("sector2back",0)
+        for i in range(301, 319):  
+            mappedSectorsToPos[str(i)]=("sector3back",0)
+        game.updateTileList(mappedSectorsToPos)
+        await SetupCommands.showGame(interaction,game) 
 
 
     @app_commands.command(name="new_game")
@@ -74,21 +128,30 @@ class SetupCommands(commands.GroupCog, name="setup"):
         await interaction.guild.create_text_channel(f'aeb{config.game_number}')
         await interaction.response.send_message('New game created!')
 
+    @app_commands.command(name="explore_tile")
+    async def explore_tile(self, interaction: discord.Interaction, tileposition: int):
+        game = GamestateHelper(interaction.channel)
+        ring = int(tileposition/100)
+        tileName = game.retrieveTileFromList(ring)
+        await interaction.response.defer(thinking=True)  
+        context = Image.new("RGBA",(345, 299),(255,255,255,0))
+        tileImage = game.showTile(tileName)
+        context.paste(tileImage,(0,0),mask=tileImage)
+        bytes = BytesIO()
+        context.save(bytes,format="PNG")
+        bytes.seek(0)
+        file = discord.File(bytes,filename="tileImage.png")
+        await interaction.followup.send(file=file)
+        view = View()
+        button = Button(label="Place Tile",style=discord.ButtonStyle.success, custom_id="placeTile_"+str(tileposition)+"_"+tileName)
+        button2 = Button(label="Discard Tile",style=discord.ButtonStyle.danger, custom_id="discardTile")
+        view.add_item(button)
+        view.add_item(button2)
+        await interaction.channel.send(view=view)
+
+
     @app_commands.command(name="add_player")
-    @app_commands.choices(faction=[
-        app_commands.Choice(name="Hydran Progress", value="hyd"),
-        app_commands.Choice(name="Eridian Empire", value="eri"),
-        app_commands.Choice(name="Orian Hegemony", value="ori"),
-        app_commands.Choice(name="Descendants of Draco", value="dra"),
-        app_commands.Choice(name="Mechanema", value="mec"),
-        app_commands.Choice(name="Planta", value="pla"),
-        app_commands.Choice(name="Terran Alliance", value="ter1"),
-        app_commands.Choice(name="Terran Conglomerate", value="ter2"),
-        app_commands.Choice(name="Terran Directorate", value="ter3"),
-        app_commands.Choice(name="Terran Federation", value="ter4"),
-        app_commands.Choice(name="Terran Republic", value="ter5"),
-        app_commands.Choice(name="Terran Union", value="ter6"),
-    ])
+    @app_commands.choices(faction=factionChoices)
     async def add_player(self, interaction: discord.Interaction, player: discord.Member, faction: app_commands.Choice[str]):
 
         game = GamestateHelper(interaction.channel)
