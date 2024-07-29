d8706a1224c539a85840d05b9a22b49978b3f3d3
Fingon00 <britainfan234@gmail.com>
2024-07-29 16:11:26 -0400

    Add remove units/influence and display faction info

--- helpers/GamestateHelper.py
+++ helpers/GamestateHelper.py
@@ -37,13 +37,18 @@ class GamestateHelper:
         self.update()
         return tile
 
+
+    def getShipFullName(self, shipAbbreviation):
+        if shipAbbreviation == "int":  
+            return "interceptor"
+        elif shipAbbreviation == "cru": 
+            return "cruiser"
+        elif shipAbbreviation == "dre": 
+            return "dreadnought"
+        elif shipAbbreviation == "sta": 
+            return "starbase"
     
     def drawTile(self, context, position, tileName, rotation):
-        configs = Properties()
-        with open("data/tileImageCoordinates.properties", "rb") as f:
-            configs.load(f)
-        x = int(configs.get(position)[0].split(",")[0])
-        y = int(configs.get(position)[0].split(",")[1])
         filepath = "images/resources/hexes/defended/"+tileName+".png"
         if not os.path.exists(filepath):  
             filepath = "images/resources/hexes/homesystems/"+tileName+".png"
@@ -57,16 +62,92 @@ class GamestateHelper:
             filepath = "images/resources/hexes/sector3/"+tileName+".png"
         if os.path.exists(filepath):  
             tileImage = Image.open(filepath).convert("RGBA")
-            tileImage = tileImage.rotate(rotation)
-            tileImage = tileImage.resize((345, 299))
             
+            tileImage = tileImage.resize((345, 299))
+            tile = self.gamestate["board"][position]
+            if "player_ships" in tile:
+                ships = tile["player_ships"]
+                count = 0
+                for ship in ships:  
+                    result = ship.split('-')
+                    color = result[0]
+                    type = self.getShipFullName(result[1])
+                    shippath = "images/resources/components/basic_ships/"+type+"_basic_"+color+".png"
+                    shipImage = Image.open(shippath).convert("RGBA")
+                    shipImage = shipImage.resize((70,70))
+                    tileImage.paste(shipImage, (125+count,32+count),mask=shipImage)
+                    count = count + 20
+                    
+            if "owner" in tile and tile["owner"] != 0:
+                color = tile["owner"]
+                influencepath = "images/resources/components/all_boards/influence_disc_"+color+".png"
+                influenceImage = Image.open(influencepath).convert("RGBA")
+                influenceImage = influenceImage.resize((50,50))
+                tileImage.paste(influenceImage, (148,125),mask=influenceImage)
+
+            tileImage = tileImage.rotate(rotation)
             font = ImageFont.truetype("arial.ttf", size=45) 
             text = str(position)
             text_position = (255, 132) 
             text_color = (255, 255, 255) 
             textDrawableImage = ImageDraw.Draw(tileImage)
             textDrawableImage.text(text_position,text,text_color,font=font)
-            context.paste(tileImage,(x,y),mask=tileImage)
+            return tileImage
+
+        return context
+    
+    def getShortFactionNameFromFull(self, fullName):
+        if fullName == "Descendants of Draco":  
+            return "draco"
+        elif fullName == "Mechanema": 
+            return "mechanema"
+        elif fullName == "Planta": 
+            return "planta"
+        elif fullName == "Orian Hegemony" or fullName == "Orion Hegemony": 
+            return "orion"
+        elif fullName == "Hydran Progress": 
+            return "hydran"
+        elif fullName == "Eridian Empire": 
+            return "eridani"
+        elif "terran" in fullName: 
+            return fullName.lower().replace(" ","_")
+
+    def drawPlayerArea(self, player):
+        faction = player["name"]
+        faction = self.getShortFactionNameFromFull(faction)
+        filepath = "images/resources/components/factions/"+faction+"_board.png"
+        context = Image.new("RGBA",(1200,500),(255,255,255,0))
+        boardImage = Image.open(filepath).convert("RGBA")
+        boardImage = boardImage.resize((895, 500))
+        context.paste(boardImage, (0,0))
+        x = 925
+        y = 50
+        font = ImageFont.truetype("arial.ttf", size=90) 
+        stroke_color=(0, 0, 0)
+        stroke_width=2
+
+        moneyImage = Image.open("images/resources/components/resourcesymbols/money.png").convert("RGBA")
+        moneyImage = moneyImage.resize((100, 100))
+        context.paste(moneyImage, (x,y))
+        text_color = (255, 255, 0) 
+        textDrawableImage = ImageDraw.Draw(context)
+        textDrawableImage.text((x+120, y),str(player["money"]),text_color,font=font,stroke_width=stroke_width,stroke_fill=stroke_color)
+
+        y = y+100
+        scienceImage = Image.open("images/resources/components/resourcesymbols/science.png").convert("RGBA")
+        scienceImage = scienceImage.resize((100, 100))
+        context.paste(scienceImage, (x,y))
+        text_color = (255,192,203) 
+        textDrawableImage = ImageDraw.Draw(context)
+        textDrawableImage.text((x+120, y),str(player["science"]),text_color,font=font,stroke_width=stroke_width,stroke_fill=stroke_color)
+
+        y = y+100
+        materialImage = Image.open("images/resources/components/resourcesymbols/material.png").convert("RGBA")
+        materialImage = materialImage.resize((100, 100))
+        context.paste(materialImage, (x,y))
+        text_color = (101, 67, 33) 
+        textDrawableImage = ImageDraw.Draw(context)
+        textDrawableImage.text((x+120, y),str(player["materials"]),text_color,font=font,stroke_width=stroke_width,stroke_fill=stroke_color)
 
         return context
 
@@ -96,9 +177,71 @@ class GamestateHelper:
         self.update()
 
 
+    def addControl(self, color, position):
+        self.gamestate["board"][position]["owner"]= color
+        self.update()
+    
+    def removeControl(self, position):
+        self.gamestate["board"][position]["owner"]= 0
+        self.update()
+
+    def addUnits(self, color, unitList, position):
+        result = unitList.strip().split(',')
+        for unit in result:
+            amount = 1
+            unit = unit.strip()
+            ship = unit
+
+            if " " in unit:
+                amount = unit.split(" ")[0]
+                ship = unit.split(" ")[1]
+            for x in range(int(amount)):
+                if ship == "interceptor" or ship == "inter" or ship == "intercepter" or ship == "i":
+                    ship = "int"
+                if ship == "dread" or ship == "dreadnought" or ship == "dreadnaught" or ship == "d":
+                    ship = "dre"
+                if ship == "cruiser" or ship == "cruise" or ship == "cruis" or ship == "c":
+                    ship = "cru"
+                if ship == "star" or ship == "s" or ship == "starbase" or ship == "base":
+                    ship = "sta"
+                if ship != "sta" and ship != "dre" and ship != "cru" and ship != "int":
+                    continue
+                unitName = color+"-"+ship
+                
+                if "player_ships" in self.gamestate["board"][position]:
+                    self.gamestate["board"][position]["player_ships"].append(unitName)
+                    
+                else:
+                    self.gamestate["board"][position]["player_ships"]= [unitName]
+        self.update()
+
+    def removeUnits(self, color, unitList, position):
+        result = unitList.strip().split(',')
+        for unit in result:
+            amount = 1
+            unit = unit.strip()
+            ship = unit
+            if " " in unit:
+                amount = unit.split(" ")[0]
+                ship = unit.split(" ")[1]
+            for x in range(int(amount)):
+                if ship == "interceptor" or ship == "inter" or ship == "intercepter" or ship == "i":
+                    ship = "int"
+                if ship == "dread" or ship == "dreadnought" or ship == "dreadnaught" or ship == "d":
+                    ship = "dre"
+                if ship == "cruiser" or ship == "cruise" or ship == "cruis" or ship == "c":
+                    ship = "cru"
+                if ship == "star" or ship == "s" or ship == "starbase" or ship == "base":
+                    ship = "sta"
+                if ship != "sta" and ship != "dre" and ship != "cru" and ship != "int":
+                    continue
+                unitName = color+"-"+ship
+                if "player_ships" in self.gamestate["board"][position]:
+                    self.gamestate["board"][position]["player_ships"].remove(unitName)
+                
+        self.update()
+
     def player_setup(self, player_id, faction, color):
-
-
         if self.gamestate["setup_finished"] == 1:
             return ("The game has already been setup!")
 
