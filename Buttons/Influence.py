9a546361106e1841ee6ca8f502da3608b3f93e15
Fingon00 <britainfan234@gmail.com>
2024-08-16 17:18:49 -0400

    Update Influence.py

--- Buttons/Influence.py
+++ Buttons/Influence.py
@@ -1,86 +1,48 @@
 import discord
 from discord.ext import commands
 from discord.ui import View
+from Buttons.Explore import ExploreButtons
 from Buttons.Population import PopulationButtons
 from helpers.GamestateHelper import GamestateHelper
 from helpers.PlayerHelper import PlayerHelper
 from helpers.DrawHelper import DrawHelper
 from discord.ui import View, Button
+from io import BytesIO
+from PIL import Image, ImageDraw, ImageFont
+from jproperties import Properties
 
-class TurnButtons:
-    @staticmethod  
-    def getPlayerFromHSLocation(game: GamestateHelper, location):  
-        tileID = game.get_gamestate()["board"][location]["sector"]  
-        return next((player for player in game.get_gamestate()["players"] if str(game.get_gamestate()["players"][player]["home_planet"]) == tileID), None)  
-
-    @staticmethod  
-    def getLocationFromID(game, id):  
-        return next((tile for tile in game.get_gamestate()["board"] if game.get_gamestate()["board"][tile]["sector"] == str(id)), None)
-    
-    @staticmethod
-    def getNextPlayer(player, game: GamestateHelper):
-        listHS = [201,203,205,207,209,211]
-        playerHSID = player["home_planet"]
-        tileLocation = int(TurnButtons.getLocationFromID(game, playerHSID))
-        index = listHS.index(tileLocation)  
-        if index is None:  
-            return None 
-        newList = listHS[index+1:] + listHS[:index] + [listHS[index]] 
-        for number in newList:
-            nextPlayer = TurnButtons.getPlayerFromHSLocation(game, str(number))
-            if nextPlayer is not None and not game.get_gamestate()["players"].get(nextPlayer, {}).get("passed", False):  
-                return game.get_gamestate()["players"][nextPlayer]
-        return None
-    
-    @staticmethod
-    async def restartTurn(player, game, interaction: discord.Interaction):
-        view = TurnButtons.getStartTurnButtons(game, player)
-        await interaction.response.send_message(player["player_name"]+ " use buttons to do your turn",view=view)
-        await interaction.message.delete()
-
-    @staticmethod
-    async def endTurn(player, game, interaction: discord.Interaction):
-        nextPlayer = TurnButtons.getNextPlayer(player,game)
-        if nextPlayer != None:
-            view = TurnButtons.getStartTurnButtons(game,nextPlayer)
-            await interaction.response.send_message(nextPlayer["player_name"]+ " use buttons to do your turn",view=view)
-        else:
-            await interaction.response.send_message("All players have passed")
-        await interaction.message.delete()
-
-    @staticmethod
-    async def passForRound(player, game: GamestateHelper, interaction: discord.Interaction, player_helper : PlayerHelper):
-        player_helper.passTurn()
-        game.update_player(player_helper)
-        nextPlayer = TurnButtons.getNextPlayer(player,game)
-        if nextPlayer != None:
-            view = TurnButtons.getStartTurnButtons(game,nextPlayer)
-            await interaction.response.send_message(nextPlayer["player_name"]+ " use buttons to do your turn",view=view)
-        else:
-            await interaction.response.send_message("All players have passed")
-        await interaction.message.delete()
-    
-
-    @staticmethod
-    async def showGame(game: GamestateHelper, interaction: discord.Interaction):
-        await interaction.response.defer(thinking=True)
-        drawing = DrawHelper(game.gamestate)
-        await interaction.followup.send(file=drawing.show_game())
-        view = View()
-        button = Button(label="Show Game",style=discord.ButtonStyle.primary, custom_id="showGame")
-        view.add_item(button)
-        await interaction.channel.send(view=view)
+class InfluenceButtons:
 
     @staticmethod  
-    def getStartTurnButtons(game: GamestateHelper,p1):
-        view = View()  
-        view.add_item(Button(label=f"Explore ({p1['explore_apt']})", style=discord.ButtonStyle.success, custom_id="startExplore"))  
-        view.add_item(Button(label=f"Research ({p1['research_apt']})", style=discord.ButtonStyle.primary, custom_id="startResearch"))  
-        view.add_item(Button(label=f"Build ({p1['build_apt']})", style=discord.ButtonStyle.success, custom_id="startBuild"))  
-        view.add_item(Button(label=f"Upgrade ({p1['upgrade_apt']})", style=discord.ButtonStyle.primary, custom_id="startUpgrade"))  
-        view.add_item(Button(label=f"Move ({p1['move_apt']})", style=discord.ButtonStyle.success, custom_id="startMove"))  
-        view.add_item(Button(label=f"Influence ({p1['influence_apt']})", style=discord.ButtonStyle.secondary, custom_id="startInfluence"))  
-        view.add_item(Button(label="Pass", style=discord.ButtonStyle.red, custom_id=f"FCID{p1["color"]}_passForRound"))
-        if len(PopulationButtons.findEmptyPopulation(game,p1)) > 0 and p1["colony_ships"] > 0:
-            view.add_item(Button(label="Put Down Population", style=discord.ButtonStyle.gray, custom_id=f"FCID{p1["color"]}_startPopDrop"))
-        return view
+    def areTwoTilesAdjacent(game: GamestateHelper, tile1, tile2, configs):  
+
+        def is_adjacent(tile_a, tile_b):  
+            for index, adjTile in enumerate(configs.get(tile_a)[0].split(",")):  
+                tile_orientation_index = (index + 6 + int(game.get_gamestate()["board"][tile_a]["orientation"] / 60)) % 6  
+                if adjTile == tile_b and tile_orientation_index in game.get_gamestate()["board"][tile_a]["wormholes"]:   
+                    return True  
+            return False  
+        
+        return is_adjacent(tile1, tile2) and is_adjacent(tile2, tile1)  
+
+    @staticmethod  
+    def getTilesToInfluence(game: GamestateHelper, player):  
+        configs = Properties()  
+        with open("data/tileAdjacencies.properties", "rb") as f:  
+            configs.load(f)  
+        tilesViewed = []
+        tilesToInfluence = []
+        playerTiles = ExploreButtons.getListOfTilesPlayerIsIn(game, player)  
+        for tile in playerTiles:  
+            for adjTile in configs.get(tile)[0].split(","):  
+                if adjTile not in tilesViewed and InfluenceButtons.areTwoTilesAdjacent(game, tile, adjTile, configs):
+                    tilesViewed.append(adjTile)
+                    if "owner" in game.get_gamestate()["board"][adjTile] and game.get_gamestate()["board"][adjTile]["owner"]==0:
+                        tilesToInfluence.append(adjTile)
+            if tile not in tilesViewed:
+                    tilesViewed.append(tile)
+                    if "owner" in game.get_gamestate()["board"][tile] and game.get_gamestate()["board"][tile]["owner"]==0:
+                        tilesToInfluence.append(tile)
+        return tilesToInfluence
+    @staticmethod  
+    def getTilesToInfluence(game: GamestateHelper, player):  
