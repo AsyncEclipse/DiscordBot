abe43e9f45eeb248e51de60cdb0a89829ff9d1b0
Fingon00 <britainfan234@gmail.com>
2024-11-17 06:47:10 -0500

    Fixes

--- helpers/GamestateHelper.py
+++ helpers/GamestateHelper.py
@@ -653,6 +653,8 @@ class GamestateHelper:
                         self.gamestate["board"][position][i][val] = num-1
                         found = True
                         break
+            else:
+                found = True
             if found:
                 if not graveYard:
                     if "neutral" not in i and "orbital" not in i and self.gamestate["players"][playerID][i.replace("adv","")+"_cubes"] < 13:
@@ -693,14 +695,21 @@ class GamestateHelper:
         
         for player in self.gamestate["players"]:
             p1 = PlayerHelper(player, self.get_player(player))
-            neutrals = p1.upkeep()
+            neutrals, orbitals = p1.upkeep()
+            for cube in range(orbitals):
+                view=View()
+                planetTypes = ["money","science"]
+                for planetT in planetTypes:
+                    if p1.stats[planetT+"_pop_cubes"] < 12:
+                        view.add_item(Button(label=planetT.capitalize(), style=discord.ButtonStyle.blurple, custom_id=f"FCID{p1.stats['color']}_addCubeToTrack_"+planetT))
+                await interaction.channel.send( f"{p1.stats['player_name']} An orbital cube was found in your graveyard, please tell the bot what track you want it to go on", view=view)
             for cube in range(neutrals):
                 view=View()
                 planetTypes = ["money","science","material"]
                 for planetT in planetTypes:
                     if p1.stats[planetT+"_pop_cubes"] < 12:
                         view.add_item(Button(label=planetT.capitalize(), style=discord.ButtonStyle.blurple, custom_id=f"FCID{p1.stats['color']}_addCubeToTrack_"+planetT))
-                await interaction.channel.send( f"{p1.stats['player_name']} A neutral or orbital cube was found in your graveyard, please tell the bot what track you want it to go on", view=view)
+                await interaction.channel.send( f"{p1.stats['player_name']} A neutral cube was found in your graveyard, please tell the bot what track you want it to go on", view=view)
 
         tech_draws = self.gamestate["player_count"]+3
         while tech_draws > 0:
