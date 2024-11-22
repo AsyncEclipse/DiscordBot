cd789d9226511931c976dae3b9ff5ba2f288f0a6
Fingon00 <britainfan234@gmail.com>
2024-11-21 19:27:03 -0500

    Pink and brown

--- commands/player_commands.py
+++ commands/player_commands.py
@@ -25,7 +25,9 @@ class PlayerCommands(commands.GroupCog, name="player"):
         app_commands.Choice(name="Yellow", value="yellow"),
         app_commands.Choice(name="Purple", value="purple"),
         app_commands.Choice(name="Green", value="green"),
-        app_commands.Choice(name="White", value="white")
+        app_commands.Choice(name="White", value="white"),
+        app_commands.Choice(name="Pink", value="pink"),
+        app_commands.Choice(name="Brown", value="brown")
     ]
 
     @app_commands.command(name="stats", description="Anything to do with player stats")
